39c1a8c14269e4b96511cc8c3537409c3c7eca9a
blake park <blkpark@blkpark.com>
2020-09-24 03:57:59 +0900

    updated channels.

--- api/businesschannel/channels.go
+++ api/businesschannel/channels.go
@@ -6,16 +6,17 @@ import (
 	"github.com/blkpark/naver-searchad-api/searchad"
 )
 
+const BASE string = "/ncc/channels"
+
 type Site struct {
 	Site string `json:"site"`
 }
 
-const BASE string = "/ncc/channels"
-
+// BusinessChannel
 type BusinessChannel struct {
 	ChannelTp         string `json:"channelTp,omitempty"`         // post
 	Name              string `json:"name,omitempty"`              // post
-	InspectRequestMsg string `json:"inspectRequestMsg,omitempty"` //post
+	InspectRequestMsg string `json:"inspectRequestMsg,omitempty"` // post
 	BusinessInfo      Site   `json:"businessInfo,omitempty"`      // post
 	// nidAut
 	// nidSes
@@ -37,13 +38,24 @@ type BusinessChannel struct {
 	StatusReason         string `json:"statusReason,omitempty"`
 }
 
-// GetBusinessChannel
-func GetBusinessChannel(params url.Values) []byte {
+// List
+func List(params url.Values) []byte {
 	api := BASE
 	return searchad.GetAPI(api, params)
 }
 
-func PostBusinessChannel(params url.Values, payload interface{}) []byte {
+func Get(nccBusinessChannelID string) []byte {
+	api := BASE + "/" + nccBusinessChannelID
+	return searchad.GetAPI(api, nil)
+}
+
+func Create(params url.Values, payload interface{}) []byte {
 	api := BASE
 	return searchad.PostAPI(api, params, payload)
+}
+
+func Update(params url.Values, payload interface{}, businessChannelID string) []byte {
+	api := BASE + "/" + businessChannelID
+	return searchad.PutAPI(api, params, payload)
+
 }
--- api/businesschannel/channels_test.go
+++ api/businesschannel/channels_test.go
@@ -1,22 +1,45 @@
 package businesschannel
 
 import (
+	"encoding/json"
+	"errors"
+	"log"
 	"net/url"
+	"reflect"
 	"testing"
 
 	"github.com/blkpark/naver-searchad-api/searchad"
 )
 
 func TestGetBusinessChannel(t *testing.T) {
-	list()
-	create()
+	created := create()
+	finded := get(created.NccBusinessChannelID)
+
+	if !reflect.DeepEqual(created.NccBusinessChannelID, finded.NccBusinessChannelID) {
+		msg := "\nbusiness Channel is not equel" + "\ncreated:" + created.NccBusinessChannelID + "\nfinded" + finded.NccBusinessChannelID
+		err := errors.New(msg)
+		log.Fatal(msg)
+		panic(err)
+	}
+
+	marshaled, _ := json.Marshal(finded)
+	updated := update(marshaled)
+	findAgain := get(created.NccBusinessChannelID)
+
+	// check update
+	if !reflect.DeepEqual(updated.Name, findAgain.Name) {
+		msg := "\nbusiness channel name not changed.\nupdated:" + updated.Name + "\nfind again:" + findAgain.Name
+		err := errors.New(msg)
+		log.Fatal(msg)
+		panic(err)
+	}
 }
 
 // GET /ncc/channels
 // https://naver.github.io/searchad-apidoc/#/operations/GET/~2Fncc~2Fchannels
 func list() {
 	params := url.Values{}
-	r := GetBusinessChannel(params)
+	r := List(params)
 	if len(r) < 1 {
 		panic(r)
 	}
@@ -25,24 +48,47 @@ func list() {
 
 // POST /ncc/channels
 // https://naver.github.io/searchad-apidoc/#/operations/POST/~2Fncc~2Fchannels
-func create() {
-	b := BusinessChannel{}
-	b.ChannelTp = "SITE"
-	b.Name = "bc_" + searchad.RandomString(8)
-	b.InspectRequestMsg = "test msg"
+func create() BusinessChannel {
+	newCh := BusinessChannel{}
+	newCh.ChannelTp = "SITE"
+	str := searchad.RandomString(8)
+	newCh.Name = "bc_" + str
+	newCh.InspectRequestMsg = "test msg"
 
-	m := Site{}
-	m.Site = "http://www.my-site.com"
-	b.BusinessInfo = m
+	site := Site{}
+	site.Site = "http://www." + str + ".com"
+	newCh.BusinessInfo = site
 
-	r := PostBusinessChannel(nil, b)
+	r := Create(nil, newCh)
 	searchad.PrintJSON(r)
+
+	json.Unmarshal(r, &newCh)
+
+	return newCh
 }
 
 // PUT /ncc/channels/{businessChannelId}{?fields}
 // https://naver.github.io/searchad-apidoc/#/operations/PUT/~2Fncc~2Fchannels~2F%7BbusinessChannelId%7D%7B%3Ffields%7D
-func update() {
-	// TODO
+func update(b []byte) BusinessChannel {
+
+	// unmarshal
+	bc := BusinessChannel{}
+	json.Unmarshal(b, &bc)
+
+	// change name
+	bc.Name = "changed_" + bc.Name
+
+	// set params
+	params := url.Values{}
+	params.Add("fields", "name")
+
+	r := Update(params, bc, bc.NccBusinessChannelID)
+	searchad.PrintJSON(r)
+
+	rbc := BusinessChannel{}
+	json.Unmarshal(r, &rbc)
+
+	return rbc
 }
 
 // PUT /ncc/channels/{businessChannelId}/inspect
@@ -75,10 +121,13 @@ func listByIds() {
 	// TODO
 }
 
-// GET /ncc/channels/{businessChannelId}
+// GET /ncc/channels/}{businessChannelId
 // https://naver.github.io/searchad-apidoc/#/operations/GET/~2Fncc~2Fchannels~2F%7BbusinessChannelId%7D
-func get() {
-	// TODO
+func get(businessChannelID string) BusinessChannel {
+	r := Get(businessChannelID)
+	b := BusinessChannel{}
+	json.Unmarshal(r, &b)
+	return b
 }
 
 // GET /ncc/purchasable-place-channels
